dc4e38112dd541abc7fb9fd0fa54648eb647d895
kedzkiest <kaede12kazu22@gmail.com>
2022-08-21 19:45:19 +0900

    created basic part of the app

--- RelationshipBetweenUs/src/App.svelte
+++ RelationshipBetweenUs/src/App.svelte
@@ -1,15 +1,55 @@
 <script>
-	const messages = ['Hello!', 'Svelte!'];
-	let current = 0;
+	let depth = [
+		{id: 0, text: "0"},
+		{id: 1, text: "1"},
+		{id: 2, text: "2"},
+		{id: 3, text: "3"}
+	];
 
-	const toggle = () => current = (current+1) % 2;
+	let selected = {id: 0, text: null};
+
+	//$: console.log(selected);
+
+	let text = "あなた";
 </script>
 
 <main>
-	{messages[current]}
-	<button on:click={toggle}>Toggle Message</button>
+	<h1>あなたと誰かの関係性</h1>
+
+	<h2>
+		関係の深さ
+
+		<select bind:value={selected}>
+			{#each depth as depth}
+				<option value={depth}>
+					{depth.text}
+				</option>
+			{/each}
+		</select>
+	</h2>
+
+	<h3>
+		{#if selected.id === 0}
+			{text}は
+		{:else}
+			{text}の
+		{/if}
+	</h3>
 </main>
 
 <style>
-	
+	h1{
+		font-weight: normal;
+		text-align: center;
+	}
+
+	h2{
+		font-weight: normal;
+		text-align: center;
+	}
+
+	h3{
+		font-weight: normal;
+		text-align: center;
+	}
 </style>
