6786d77f4036643253e67df87dd3ceb426a2b8b9
kedzkiest <kaede12kazu22@gmail.com>
2022-08-21 17:51:39 +0900

    trying Svelte for new web app

---
+++ RelationshipBetweenUs/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	const messages = ['Hello!', 'Svelte!'];
+	let current = 0;
+
+	const toggle = () => current = (current+1) % 2;
+</script>
+
+<main>
+	{messages[current]}
+	<button on:click={toggle}>Toggle Message</button>
+</main>
+
+<style>
+	
+</style>
