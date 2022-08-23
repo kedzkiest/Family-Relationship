94641574ff7cec947467f35a5f8497c069359a02
kedzkiest <kaede12kazu22@gmail.com>
2022-08-23 15:14:51 +0900

    completed system in case connection depth is 0

--- RelationshipBetweenUs/src/App.svelte
+++ RelationshipBetweenUs/src/App.svelte
@@ -6,38 +6,87 @@
 		{id: 3, text: "3"}
 	];
 
-	let selected = {id: 0, text: null};
+	let selected = {id: null, text: null};
 
 	//$: console.log(selected);
 
 	let text = "あなた";
+
+	let showResult = false;
+	const seeResult = () => showResult = true;
+
+	function handleReload(){
+
+	}
 </script>
 
 <main>
-	<h1>あなたと誰かの関係性</h1>
+	
+	<div class="upperPart">
+		<h1>あなたと誰かの関係性</h1>
 
-	<h2>
-		関係の深さ
+		{#if !showResult}
+			<h2>
+				関係の深さ
 
-		<select bind:value={selected}>
-			{#each depth as depth}
-				<option value={depth}>
-					{depth.text}
-				</option>
-			{/each}
-		</select>
-	</h2>
+				<select bind:value={selected}>
+					{#each depth as depth}
+						<option value={depth}>
+							{depth.text}
+						</option>
+					{/each}
+				</select>
+			</h2>
 
-	<h3>
-		{#if selected.id === 0}
-			{text}は
-		{:else}
-			{text}の
+			{#if selected.id === 0}
+				<div class="middlePart">
+					<button on:click={seeResult}>結果を見る</button>
+				</div>
+			{/if}
 		{/if}
-	</h3>
+
+		<h3>
+			{#if selected.id === null}
+				<div></div>
+			{:else if selected.id === 0}
+				{text}は
+			{:else}
+				{text}の
+			{/if}
+		</h3>
+	</div>
+
+
+	{#if showResult}
+		<div class="lowerPart">
+			<h2>{text}です。</h2>
+		</div>
+
+		<div class="bottomPart">
+			<form on:reload={handleReload}>
+				<button type="reload">もう一度診断する</button>
+			</form>
+		</div>
+	{/if}
 </main>
 
 <style>
+	.upperPart{
+		background-color: #ffdddd;
+	}
+
+	.middlePart{
+		text-align: center;
+	}
+
+	.lowerPart{
+		background-color: #ddffdd;
+	}
+
+	.bottomPart{
+		text-align: center;
+	}
+
 	h1{
 		font-weight: normal;
 		text-align: center;
