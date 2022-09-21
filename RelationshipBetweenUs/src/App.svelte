3a68b04114f5221159137d6dfd6c4efb6db07794
kedzkiest <kaede12kazu22@gmail.com>
2022-09-21 13:25:56 +0900

    reduced depth by 1

--- RelationshipBetweenUs/src/App.svelte
+++ RelationshipBetweenUs/src/App.svelte
@@ -3,7 +3,6 @@
 		{id: 0, text: "0"},
 		{id: 1, text: "1"},
 		{id: 2, text: "2"},
-		{id: 3, text: "3"}
 	];
 
 	let selectedDepth = {id: null, text: null};
@@ -24,8 +23,19 @@
 	let selectedRelationship = [
 		{id: null, text: null},
 		{id: null, text: null},
-		{id: null, text: null}
 	];
+
+	let ansRelationship = "";
+	$: switch(selectedRelationship[0].id){
+		// 父
+		case 0:
+			switch(selectedRelationship[1].id){
+				// 父の父
+				case 0:
+					
+			}
+
+	}
 
 	//$: console.log(selectedRelationship);
 	let allSelected = false;
@@ -110,38 +120,6 @@
 				</select>
 
 				は
-			{:else if selectedDepth.id === 3}
-				あなたの
-
-				<select bind:value={selectedRelationship[0]}>
-					{#each relationship as r}
-						<option value={r}>
-							{r.text}
-						</option>
-					{/each}
-				</select>
-
-				の
-
-				<select bind:value={selectedRelationship[1]}>
-					{#each relationship as r}
-						<option value={r}>
-							{r.text}
-						</option>
-					{/each}
-				</select>
-
-				の
-
-				<select bind:value={selectedRelationship[2]}>
-					{#each relationship as r}
-						<option value={r}>
-							{r.text}
-						</option>
-					{/each}
-				</select>
-
-				は
 			{/if}
 		</h3>
 	</div>
@@ -161,8 +139,6 @@
 				<h2>あなたの{selectedRelationship[0].text}です。</h2>
 			{:else if selectedDepth.id === 2}
 				<h2>あなたの{selectedRelationship[0].text}の{selectedRelationship[1].text}です。</h2>
-			{:else if selectedDepth.id === 3}
-				<h2>あなたの{selectedRelationship[0].text}の{selectedRelationship[1].text}の{selectedRelationship[2].text}です。</h2>
 			{/if}
 		</div>
 
