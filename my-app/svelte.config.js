be256e3af4cdeedd65e9fed858ab9392b62bbefd
kedzkiest <kaede12kazu22@gmail.com>
2022-09-21 18:07:53 +0900

    changed for static hosting

---
+++ my-app/svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		paths: {
+			base: dev ? '' : '/Family_Relationship',
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+		}),
+		appDir: 'internal',
+	}
+};
+
+export default config;
